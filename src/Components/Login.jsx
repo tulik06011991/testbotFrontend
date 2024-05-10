87511eeae3c79df217bce80135e951af3515fcbf
tulik <tolqinmirsaliyev@gmail.com>
2024-05-10 15:07:56 -0500

    ffgrt

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -56,7 +56,7 @@ const Login = () => {
     <div>
       
       <div className="h-screen bg-indigo-100 flex justify-center items-center">
-        <div className="lg:w-2/5 md:w-1/2 w-2/3 ">
+        <div className="lg:w-2/5 md:w-1/2 w-10/12 ">
           <form  onSubmit={handleSubmitt} className="bg-white p-10 rounded-lg shadow-lg min-w-full">
             <h1 className="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">Login</h1>
             
@@ -66,7 +66,7 @@ const Login = () => {
             </div>
             <div>
               <label className="text-gray-800 font-semibold block my-3 text-md" htmlFor="password">Password</label>
-              <input   onChange={(e) => setPassword(e.target.value)} className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" placeholder="password" />
+              <input  type='password'  onChange={(e) => setPassword(e.target.value)} className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"  name="password" id="password" placeholder="password" />
             </div>
             <button type="submit" className="w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">Login</button>
           </form>
