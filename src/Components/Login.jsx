28edf340bf276cfd3f880f7a8b373517ee8ad232
tulik <tolqinmirsaliyev@gmail.com>
2024-05-11 13:14:52 -0500

    dfdfd

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
             "Access-Control-Allow-Methods": "OPTIONS, GET, PUT POST",
             "Access-Control-Allow-Headers": "*"
           },
-          withCredentials: true
+         
         },
       );
 
