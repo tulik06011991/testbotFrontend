4e717e8fa8e95bd5c29a972cd1018a16db2f6a7c
tulik <tolqinmirsaliyev@gmail.com>
2024-05-11 15:29:53 -0500

    liuoilo

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist' // Ishlab chiqarilgan fayllar katalogi (masalan, 'dist')
-  },
-  server: {
-    port: 3000, // Server port
-  },
-  base: '/', // Brauzer uchun manba manzili
-  optimizeDeps: {
-    include: ['react', 'react-dom'], // So'rov qilishni optimallashtirish
-  },
+  index:{
+
+  }
 })
